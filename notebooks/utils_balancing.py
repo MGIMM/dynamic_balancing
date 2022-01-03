5c782fe541e60ba360104e87a299ebe10ff3a4a5
MGIMM <duqiming2004@gmail.com>
2022-01-03 10:45:48 +0100

    first commit

---
+++ notebooks/utils_balancing.py
@@ -0,0 +1,225 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch import nn
+from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
+
+
+# GIPWE
+class Shallow_GIPW:
+    def __init__(self, xi, xi_ring, w =None, w_ring = None, epsilon = 1e-8, test_split_ratio = 0.2, bootstrap_multiplier = 2):
+        # parameter used to avoid division by 0
+        self.epsilon = epsilon
+        # import support of source and target measures
+        self.xi = xi
+        self.xi_ = xi # backup
+        self.xi_ring = xi_ring
+        
+        self.n = self.xi.shape[0]
+        self.m = self.xi_ring.shape[0]
+        # backup
+        self.n_ = self.xi.shape[0]
+        self.m_ = self.xi_ring.shape[0]
+        # init weights
+        if w is not None:
+            self.w_bootstrap_indicator = True
+            self.w = w
+        else:
+            self.w = np.ones(self.n)
+            self.w_bootstrap_indicator = False
+        if w_ring is not None:
+            self.w_ring_bootstrap_indicator = True
+            self.w_ring = w_ring
+        else:
+            self.w_ring = np.ones(self.m)
+            self.w_ring_bootstrap_indicator = False
+        ## setup artificial supervised learning problem
+        if self.w_bootstrap_indicator:
+            self.xi = self.xi[np.random.choice(self.n, size = int(self.n*bootstrap_multiplier), p = self.w/np.sum(self.w))]
+            self.n *= bootstrap_multiplier
+        if self.w_ring_bootstrap_indicator:
+            self.xi_ring = self.xi_ring[np.random.choice(self.m, size = int(self.m*bootstrap_multiplier), p = self.w_ring/np.sum(self.w_ring))]
+            self.m *= bootstrap_multiplier
+        X = np.concatenate((self.xi,self.xi_ring), axis = 0)
+        Y = np.concatenate((np.zeros(self.n), np.ones(self.m)), axis = 0)
+        # apply a random permutation
+        random_indices = np.random.choice(self.n+self.m, self.n+self.m, replace = False)
+        X = X[random_indices]
+        Y = Y[random_indices]
+        # train/test splitting
+        self.Xtrain = X[random_indices[0:int((self.n+self.m)*test_split_ratio)],:]
+        self.Ytrain = Y[random_indices[0:int((self.n+self.m)*test_split_ratio)]]
+        self.Xtest = X[random_indices[int((self.n+self.m)*test_split_ratio)+1:],:]
+        self.Ytest = Y[random_indices[int((self.n+self.m)*test_split_ratio)+1:]]
+        
+    def train(self,model,xi, log = False):
+        model.fit(self.Xtrain, self.Ytrain.reshape((self.Xtrain.shape[0],)))
+        test_loss = np.mean((model.predict(self.Xtest) - self.Ytest)**2)
+        train_loss = np.mean((model.predict(self.Xtrain) - self.Ytrain)**2)
+        if log:
+            print("train loss:", train_loss)
+            print("test loss:", test_loss)
+        try:
+            e = model.predict_proba(xi)[:,1] 
+        except:
+            e = model.predict(xi) 
+            
+        self.weights = e/(1.-e+self.epsilon) *self.n_/self.m_
+        
+        
+        
+    
+    
+class OptimalTransportBalancing():
+    def __init__(self, metric = None):
+        """
+        Attributes
+        ----------
+        metric: function (x,y)
+            Compute distance between two elements x and y, that are both assumed to be tensors.
+        """
+        if metric != None:
+            self.metric = metric
+        else:
+            self.metric = lambda x,y : torch.linalg.norm(x-y)
+    def get_nearest_neighbor_index (self, x,Y):
+        self.n = Y.shape[0]
+        distance_list = torch.zeros(self.n)
+        for i in range(self.n):
+            distance_list[i] = self.metric(x, Y[i])
+        return torch.argmin(distance_list).item() 
+    def get_weights(self, source, target, source_weights = None, target_weights = None):
+        self.n = len(source)
+        self.m = len(target)
+        if source_weights is not None:
+            self.w = source_weights.reshape((self.n,))
+        else:
+            self.w = torch.ones(self.n)
+        if target_weights is not None:
+            self.w_ring = target_weights.reshape((self.m,))
+        else:
+            self.w_ring = torch.ones(self.m)
+        sum_nn_w_ring = torch.zeros(self.n)
+        nearest_neighbor_index = torch.zeros(self.m, dtype = int)
+        for j in range(self.m):
+            nearest_neighbor_index[j] = self.get_nearest_neighbor_index(target[j], source)
+        for i in range(self.n):
+            for j in range(self.m):
+                if int(nearest_neighbor_index[j].item()) == i:
+                    sum_nn_w_ring[i] += self.w_ring[j]
+        eta_ring = self.w * sum_nn_w_ring
+        return eta_ring *self.n/self.m
+    
+dev = "cuda:0"    
+class Deep_GIPW():
+    def __init__(self, xi, xi_ring, w = None, w_ring = None, epsilon = 1e-8, dev = dev, test_split_ratio = 0.2, bootstrap_multiplier = 2):
+        self.epsilon = epsilon
+        self.dev = dev
+        with torch.no_grad():
+            self.xi = xi.clone().to(self.dev)
+            self.xi_ring = xi_ring.clone().to(self.dev)
+            # backup
+            self.xi_ = xi.clone().to(self.dev)
+            self.xi_ring_ = xi_ring.clone().to(self.dev)
+            if w is not None:
+                self.w = w.clone().to(self.dev)
+            else:
+                self.w = torch.ones(len(xi)).clone().to(self.dev)
+            if w_ring is not None:
+                self.w_ring = w_ring.clone().to(self.dev)
+            else:
+                self.w_ring = torch.ones(len(xi_ring)).to(self.dev)
+        self.n = len(xi)
+        self.m = len(xi_ring)
+        # backup
+        self.n_ = len(xi)
+        self.m_ = len(xi_ring)
+        
+        if w is not None:
+            self.w_bootstrap_indicator = True
+            self.w = w.to(self.dev)
+        else:
+            self.w = torch.ones(self.n).to(self.dev)
+            self.w_bootstrap_indicator = False
+        if w_ring is not None:
+            self.w_ring_bootstrap_indicator = True
+            self.w_ring = w_ring.to(self.dev)
+        else:
+            self.w_ring = torch.ones(self.m).to(self.dev)
+            self.w_ring_bootstrap_indicator = False
+        ## setup artificial supervised learning problem
+        if self.w_bootstrap_indicator:
+            rand_ind = torch.multinomial(self.w,  int(self.n*bootstrap_multiplier), replacement = True)
+            self.xi = self.xi[rand_ind]
+            self.n *= bootstrap_multiplier
+        if self.w_ring_bootstrap_indicator:
+            rand_ind = torch.multinomial(self.w_ring,  int(self.m*bootstrap_multiplier), replacement = True)
+            self.xi_ring = self.xi_ring[rand_ind]
+            self.m *= bootstrap_multiplier
+        X = torch.cat((self.xi,self.xi_ring), axis = 0)
+        Y = torch.cat((torch.zeros((self.n,1)), torch.ones((self.m,1))), axis = 0)
+        # apply a random permutation
+        random_indices = np.random.choice(self.n+self.m, self.n+self.m, replace = False)
+        X = X[random_indices]
+        Y = Y[random_indices]
+        Y = Y.to(self.dev)
+        # train/test splitting
+        self.Xtrain = X[random_indices[0:int((self.n+self.m)*test_split_ratio)],:]
+        self.Ytrain = Y[random_indices[0:int((self.n+self.m)*test_split_ratio)]]
+        self.Xtest = X[random_indices[int((self.n+self.m)*test_split_ratio)+1:],:]
+        self.Ytest = Y[random_indices[int((self.n+self.m)*test_split_ratio)+1:]]
+    
+    def train(self,
+              model,
+              optimizer,
+              batch_size = 64,
+              epochs = 500):
+        loss_fn = nn.MSELoss()
+        dataset_train = TensorDataset(self.Xtrain,self.Ytrain.type(torch.float))
+        dataset_test = TensorDataset(self.Xtest,self.Ytest.type(torch.float))
+        train_dataloader = DataLoader(dataset_train, batch_size=batch_size)
+        test_dataloader = DataLoader(dataset_test, batch_size=batch_size)
+        def train_loop():
+            size = len(train_dataloader.dataset)
+            for batch, (X, y) in enumerate(train_dataloader):
+                # Compute prediction and loss
+                pred = model(X)
+                loss = loss_fn(pred, y)
+        
+                # Backpropagation
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+        
+                if batch % 100 == 0:
+                    loss, current = loss.item(), batch * len(X)
+                    #print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            return loss
+
+
+        def test_loop():
+            size = len(test_dataloader.dataset)
+            num_batches = len(test_dataloader)
+            #test_loss, correct = 0, 0
+            test_loss = 0
+        
+            with torch.no_grad():
+                for X, y in test_dataloader:
+                    pred = model(X)
+                    test_loss += loss_fn(pred, y).item()
+                    #correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        
+            test_loss /= num_batches
+            return test_loss
+        test_loss_trace = []
+        train_loss_trace = []
+        for t in range(epochs):
+            current_train_loss = train_loop()
+            current_test_loss = test_loop()
+            test_loss_trace.append(current_test_loss)
+            train_loss_trace.append(current_train_loss.to("cpu").detach().numpy())
+        return train_loss_trace, test_loss_trace
+        
+        
+        
